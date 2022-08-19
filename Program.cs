72a5c09dcf4348fe6aaafe65a1f9a866f5ac0b5b
Сорокин Алексей <jiysuan@mail.ru>
2022-08-19 09:32:54 +0400

    Оно живое... кажется

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ void SortArray(int[,] matrix)
            int position = j;
            for (int k = j+1; k < matrix.GetLength(1); k++)
            {
-                if (matrix[i, k] < matrix[i, position]) position = k;
+                if (matrix[i, k] > matrix[i, position]) position = k;
            }
            int temp = matrix[i, j];
            matrix[i, j] = matrix[i, position];
@@ -69,7 +69,6 @@ void Zadacha56()
     PrintArrayInt(matrix);
     FindMinSumString(matrix);
     Console.WriteLine();
-
 }
 
 void FindMinSumString(int[,] matrix)
@@ -92,17 +91,60 @@ void FindMinSumString(int[,] matrix)
     {   
         if (SumArray[i] < SumArray[min]) min = i;
     }
-    Console.WriteLine("Номер строки с наименьшей суммой элементов: " + min);
+    Console.WriteLine("Номер строки с наименьшей суммой элементов (подсчет для казуалов, нулевую строку считаем первой): " + (min+1));
 }
 
-
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Задача 58. Заполните спирально массив 4 на 4 числами от 1 до 16.
 
 void Zadacha58()
 {
+    Random rand = new Random();
+    int lines = 4;
+    int column = lines;
+    int[,] matrix = new int [lines, column];
+    int iStart = 0;
+    int iEnd = 0;
+    int jStart = 0;
+    int jEnd = 0;
+
+    int k = 1;
+    int i = 0;
+    int j = 0;
+
+    while (k <= lines*column)
+    {   
+        matrix[i, j] = new Random().Next(1,17);
+
+        if (i == iStart && j < column - jEnd - 1)
+            j++;
+        else if (j == column - jEnd - 1 && i < lines - iEnd - 1)
+            i++;
+        else if (i == lines - iEnd - 1 && j > jStart)
+            j--;
+        else
+            i--;
+
+        if ((i == iStart + 1) && (j == jStart) && (jStart != column - jEnd - 1))
+        {
+            iStart++;
+            iEnd++;
+            jStart++;
+            jEnd++;
+        }
+        k++;
+    }
+
+    PrintArrayInt(matrix);
+
+}
+
+// Задача 61: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void Zadacha61()
+{
     Random rand = new Random();    
-    int line = new Random().Next(5,10);
-    int column = new Random().Next(5,10);
+    int line = new Random().Next(4,7);
+    int column = line;
     int[,] matrix = new int [line, column];
     int[,] matrixReloaded = new int [line, column];
     int[,] matrixRevolutions = new int [line, column];
@@ -119,18 +161,26 @@ void Zadacha58()
 
 
 void FillArrayOp(int[,] matrix, int[,] matrixReloaded, int[,] matrixRevolutions)
-{
+{   
     for (int i = 0; i < matrixRevolutions.GetLength(0); i++)
-    {
+    {   
         for (int j = 0; j < matrixRevolutions.GetLength(1); j++)
-        {
-            int a = matrix[i, j];
-            int b = matrixReloaded[i, j];
-            matrixRevolutions[i, j] = a*b;
+        {   
+            int sum = 0;
+            for (int k = 0; k < matrixRevolutions.GetLength(1); k++)
+            {
+                int a = matrix[j, k];
+                int b = matrixReloaded[k, i];
+                sum = sum + a*b;
+                
+                matrixRevolutions[j, i] = sum;
+            }
         }
     }
 }
 
+
 //Zadacha54();
 //Zadacha56();
 //Zadacha58();
+//Zadacha61();
